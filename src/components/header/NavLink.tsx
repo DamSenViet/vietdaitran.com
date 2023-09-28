4216d85dd4ad6f079d4109c1d0748b465fab733a
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-09-28 03:23:40 -0700

    chore: leaner NavLink

--- src/components/header/NavLink.tsx
+++ src/components/header/NavLink.tsx
@@ -1,33 +1,36 @@
-import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material'
+import React from 'react'
+import NextLink from 'next/link'
+import { styled } from '@mui/material'
 import { useRouter } from 'next/router'
 
-export function NavLink(props: MuiLinkProps) {
-  const { ...other } = props
+interface NavLinkProps {
+  href: string
+  children: React.ReactNode
+}
+
+const StyledNextLink = styled(NextLink)({})
+
+export function NavLink(props: NavLinkProps) {
+  const { href, children } = props
   const router = useRouter()
-  const pathname = props.href
+  const pathname = href
   const routerPathname = router.pathname
   const shouldBeActive = pathname === routerPathname
 
-  // const isExternal =
-  //   typeof props.href === 'string' &&
-  //   (props.href.indexOf('http') === 0 || props.href.indexOf('mailto:') === 0)
-
-  const className = shouldBeActive
-    ? `${props.className}`
-    : `${props.className} 'active'`
   return (
-    <MuiLink
-      className={className}
-      {...other}
-      variant="subtitle2"
-      color="text.primary"
-      sx={(theme) => ({
-        fontWeight: shouldBeActive ? 500 : 400,
+    <StyledNextLink
+      className="active"
+      href={href}
+      sx={{
+        typography: 'subtile2',
+        color: shouldBeActive ? 'text.primary' : 'text.secondary',
         textDecoration: 'none',
         paddingX: 2,
         paddingY: 1,
         borderRadius: 100,
-      })}
-    />
+      }}
+    >
+      {children}
+    </StyledNextLink>
   )
 }
