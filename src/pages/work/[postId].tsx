143e1af503f166c9177504d023a8959911a356a0
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-09-30 05:31:30 -0700

    feat: added WorkPostHero
    - recommendedPostDatum access available

--- src/pages/work/[postId].tsx
+++ src/pages/work/[postId].tsx
@@ -3,14 +3,21 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { ParsedUrlQuery } from 'querystring'
 import MDXContent from '@/components/MDXContent'
-import { getWorkPostIds, getWorkPost, WorkPost } from '@/api/workPosts'
+import WorkPostHero from '@/components/work/WorkPostHero'
+import {
+  getWorkPostIds,
+  getWorkPost,
+  WorkPost,
+  WorkPostDatum,
+  getWorkPostDatum,
+} from '@/api/workPosts'
 
 export interface WorkPostPageParams extends ParsedUrlQuery {
   postId: string
 }
 
 export const getStaticPaths: GetStaticPaths<WorkPostPageParams> = async () => {
-  const postIds = getWorkPostIds({ visible: true })
+  const postIds = getWorkPostIds()
   const paths = postIds.map((postId) => ({ params: { postId } }))
   return {
     paths,
@@ -18,23 +25,48 @@ export const getStaticPaths: GetStaticPaths<WorkPostPageParams> = async () => {
   }
 }
 
-export interface WorkPostPageProps extends WorkPost {}
+export interface WorkPostPageProps {
+  workPost: WorkPost
+  recommendedWorkPostData: WorkPostDatum[]
+}
 
 export const getStaticProps: GetStaticProps<
   WorkPostPageProps,
   WorkPostPageParams
 > = async ({ params }) => {
-  const project = await getWorkPost(params!.postId)
-  return { props: project }
+  const workPost = await getWorkPost(params!.postId)
+
+  // calculate recommendations here
+  const postIds = getWorkPostIds()
+  const postIdsIndex = postIds.findIndex((postId) => params!.postId)
+
+  //  revolving slice
+  const recommendedPostIds = postIds.slice(
+    (postIdsIndex + 1) % postIds.length,
+    (postIdsIndex + 1 + 3) % postIds.length
+  )
+
+  const recommendedWorkPostData = recommendedPostIds.map(getWorkPostDatum)
+
+  return {
+    props: {
+      workPost,
+      recommendedWorkPostData,
+    },
+  }
 }
 
-export default function WorkPostPage({ datum, mdxSource }: WorkPostPageProps) {
+export default function WorkPostPage({
+  workPost,
+  recommendedWorkPostData,
+}: WorkPostPageProps) {
   return (
     <>
       <Head>
-        <title>{`${datum.title} - Viet Tran`}</title>
+        <title>{`${workPost.datum.title} - Viet Tran`}</title>
       </Head>
-      {mdxSource && <MDXContent mdxSource={mdxSource} />}
+      <WorkPostHero postDatum={workPost.datum} />
+      {workPost.mdxSource && <MDXContent mdxSource={workPost.mdxSource} />}
     </>
   )
 }
