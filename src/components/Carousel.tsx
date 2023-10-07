0334800814811f2dd878ebbd16cba437685f1585
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-10-07 08:20:35 -0700

    feat: carousel content alignment & background

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,14 +1,12 @@
 import React, { ComponentProps } from 'react'
 import NukaCarousel from 'nuka-carousel'
-import { IconButton } from '@mui/material'
+import { useTheme, IconButton } from '@mui/material'
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md'
 import MediaContainer from './MediaContainer'
 import withDefaultProps from '@/utils/withDefaultProps'
 import { transparentize } from 'color2k'
 
-const Carousel = withDefaultProps(NukaCarousel, {
-  adaptiveHeight: true,
-  adaptiveHeightAnimation: true,
+const CustomCarousel = withDefaultProps(NukaCarousel, {
   wrapAround: true,
   renderCenterLeftControls: ({ previousSlide, previousDisabled }) => (
     <IconButton
@@ -52,14 +50,29 @@ const Carousel = withDefaultProps(NukaCarousel, {
   ),
 })
 
-const ContainedCarousel: React.FunctionComponent<
-  ComponentProps<typeof Carousel>
-> = (props) => {
+const Carousel: React.FunctionComponent<
+  ComponentProps<typeof NukaCarousel>
+> = ({ ...props }) => {
+  const theme = useTheme()
   return (
     <MediaContainer>
-      <Carousel {...props} />
+      <CustomCarousel
+        {...props}
+        style={{
+          borderRadius: 16,
+          backgroundColor: theme.palette.background.default,
+          opacity: 1,
+          backgroundImage:
+            theme.palette.mode === 'light'
+              ? 'repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.03) 25%, transparent 25%, transparent 75%, rgba(0, 0, 0, 0.03) 75%, rgba(0, 0, 0, 0.03)), repeating-linear-gradient(45deg, rgba(0, 0, 0, 0.03) 25%, #ebe7e0 25%, #ebe7e0 75%, rgba(0, 0, 0, 0.03) 75%, rgba(0, 0, 0, 0.03))'
+              : 'repeating-linear-gradient(45deg, rgba(255,255,255,0.03) 25%, transparent 25%, transparent 75%, rgba(255,255,255,0.03) 75%, rgba(255,255,255,0.03)), repeating-linear-gradient(45deg, rgba(255,255,255,0.03) 25%, #080808 25%, #080808 75%, rgba(255,255,255,0.03) 75%, rgba(255,255,255,0.03))',
+          backgroundPosition: '0 0, 23px 23px',
+          backgroundSize: '46px 46px',
+          backgroundRepeat: 'repeat',
+        }}
+      />
     </MediaContainer>
   )
 }
 
-export default ContainedCarousel
+export default Carousel
