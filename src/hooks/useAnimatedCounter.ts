b6901c901b3ae7d406c05b11afbb489c8096810f
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-10-17 23:22:46 -0700

    feat: useAnimatedCounter delay option

--- src/hooks/useAnimatedCounter.ts
+++ src/hooks/useAnimatedCounter.ts
@@ -1,22 +1,24 @@
 import { useEffect, useState } from 'react'
 import { animate } from 'framer-motion'
+import { stepDuration } from '@/utils/animation'
 
 const useAnimatedCounter = (
   maxValue: number,
   initialValue = 0,
-  duration = 1
+  duration = stepDuration,
+  delay = 0
 ) => {
   const [counter, setCounter] = useState<number>(initialValue)
-
   useEffect(() => {
     const controls = animate(initialValue, maxValue, {
       duration,
+      delay,
       onUpdate(value) {
         setCounter(value)
       },
     })
     return () => controls.stop()
-  }, [initialValue, maxValue, duration])
+  }, [initialValue, maxValue, duration, delay])
 
   return counter
 }
