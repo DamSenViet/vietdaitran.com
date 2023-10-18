4a7cbf9e5e55b173c41574015d2a88d3904765b2
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-10-17 21:32:45 -0700

    fix: footer copyright not animating

--- src/components/footer/FooterCopyright.tsx
+++ src/components/footer/FooterCopyright.tsx
@@ -1,17 +1,11 @@
 import { Box, Typography } from '@mui/material'
 import { getYear } from 'date-fns'
 import sectionPadding from '@/components/constants/sectionPadding'
-import { motion } from 'framer-motion'
-import useMovingFade from '@/hooks/useMovingFade'
 
 export default function FooterCopyright() {
   const currentYear = getYear(new Date())
   return (
-    <Box
-      component={motion.div}
-      {...useMovingFade()}
-      sx={{ ...sectionPadding, flexGrow: 1, overflow: 'hidden' }}
-    >
+    <Box sx={{ ...sectionPadding, flexGrow: 1 }}>
       <Typography color="text.secondary" variant="body2">
         © {currentYear} Viet Tran
       </Typography>
