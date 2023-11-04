de9b370dff4e5e201c2ecb6834d4e550f51b2dd4
Viet <26016342+DamSenViet@users.noreply.github.com>
2023-11-03 23:35:01 -0700

    feat: mdx table scroll into view animation

--- src/components/markdown/Table.tsx
+++ src/components/markdown/Table.tsx
@@ -1,12 +1,14 @@
 import React, { ComponentProps } from 'react'
+import { motion } from 'framer-motion'
 import {
   TableContainer as MuiTableContainer,
   Table as MuiTable,
 } from '@mui/material'
+import useMovingFade from '@/hooks/useMovingFade'
 
 export default function Table({ children }: ComponentProps<'table'>) {
   return (
-    <MuiTableContainer>
+    <MuiTableContainer component={motion.div} {...useMovingFade()}>
       <MuiTable
         sx={(theme) => ({
           width: 'auto',
